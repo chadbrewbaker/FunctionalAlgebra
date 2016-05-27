6f18a0eee2eb74d4c8e12a0f5d8327fa1b8b9cd8
Chad Brewbaker <crb002@gmail.com>
2016-05-27 13:17:55 -0600

    update for presentation

---
+++ build.sh
@@ -0,0 +1,6 @@
+ruby curry.rb > curry.out
+ruby cocurry.rb > cocurry.out
+ruby inputSplit.rb > inputSplit.out
+ruby ignoreInput.rb > ignoreInput.out
+ruby ignoreOutput.rb >ignoreOutput.out
+pdflatex -shell-escape FunctionalAlgebra
